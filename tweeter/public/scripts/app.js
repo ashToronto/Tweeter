bc16b1b09a7ac6146a1bafa7be1fa1d6907363e4
ashwin <ashwin.ashwinkhanna@gmail.com>
2018-05-23 15:53:44 -0400

    added renderTweet function to app.js

--- tweeter/public/scripts/app.js
+++ tweeter/public/scripts/app.js
@@ -42,33 +42,99 @@ function createHeader(data) {
   return $dublicate;
  }
 
+ // function createAndAppendTweet(tweetData) {
+ //   let $tweetHtml = createTweetElement(tweet);
+ //   $('.container').append($tweetHtml);
+ // }
+
 function renderTweets(tweets){
-  for (i in tweets){
-    createTweetElement(i);
+  for (tweet of tweets){
+   let $tweetHtml = createTweetElement(tweet);
+   $('.container').append($tweetHtml);
   }
+
+  // for (let i = 0; i < tweets.length; i += 2) {
+  //   createTweetElement(tweets[i]);
+  // }
+
+  // tweets.forEach(function (tweet) {
+  //   let $tweetHtml = createTweetElement(tweet);
+  //   $('.container').append($tweetHtml);
+  // });
+
+  // tweets.forEach(createAndAppendTweet);
 }
 
+renderTweets(data);
+
 // Appends main page
-  var $tweet = createTweetElement(tweetData);
-  // Test / driver code (temporary)
-  console.log($tweet); // to see what it looks like
-  console.log($tweet.html());
-  $('.container').append($tweet); // to add it to the page so we can make sure it's got all the right elements, classes, etc.
+  // var $tweet = createTweetElement(tweetData);
+  // // Test / driver code (temporary)
+  // console.log($tweet); // to see what it looks like
+  // console.log($tweet.html());
+  // $('.container').append($tweet); // to add it to the page so we can make sure it's got all the right elements, classes, etc.
 });
 
 // STATIC TWEET DATA
- const tweetData = {
-  "user": {
-    "name": "Newton",
-    "avatars": {
-      "small":   "https://vanillicon.com/788e533873e80d2002fa14e1412b4188_50.png",
-      "regular": "https://vanillicon.com/788e533873e80d2002fa14e1412b4188.png",
-      "large":   "https://vanillicon.com/788e533873e80d2002fa14e1412b4188_200.png"
+//  const tweetData = {
+//   "user": {
+//     "name": "Newton",
+//     "avatars": {
+//       "small":   "https://vanillicon.com/788e533873e80d2002fa14e1412b4188_50.png",
+//       "regular": "https://vanillicon.com/788e533873e80d2002fa14e1412b4188.png",
+//       "large":   "https://vanillicon.com/788e533873e80d2002fa14e1412b4188_200.png"
+//     },
+//     "handle": "@SirIsaac"
+//   },
+//   "content": {
+//     "text": "If I have seen further it is by standing on the shoulders of giants"
+//   },
+//   "created_at": 1461116232227
+// }
+
+const data = [
+  {
+    "user": {
+      "name": "Newton",
+      "avatars": {
+        "small":   "https://vanillicon.com/788e533873e80d2002fa14e1412b4188_50.png",
+        "regular": "https://vanillicon.com/788e533873e80d2002fa14e1412b4188.png",
+        "large":   "https://vanillicon.com/788e533873e80d2002fa14e1412b4188_200.png"
+      },
+      "handle": "@SirIsaac"
     },
-    "handle": "@SirIsaac"
+    "content": {
+      "text": "If I have seen further it is by standing on the shoulders of giants"
+    },
+    "created_at": 1461116232227
   },
-  "content": {
-    "text": "If I have seen further it is by standing on the shoulders of giants"
+  {
+    "user": {
+      "name": "Descartes",
+      "avatars": {
+        "small":   "https://vanillicon.com/7b89b0d8280b93e2ba68841436c0bebc_50.png",
+        "regular": "https://vanillicon.com/7b89b0d8280b93e2ba68841436c0bebc.png",
+        "large":   "https://vanillicon.com/7b89b0d8280b93e2ba68841436c0bebc_200.png"
+      },
+      "handle": "@rd" },
+    "content": {
+      "text": "Je pense , donc je suis"
+    },
+    "created_at": 1461113959088
   },
-  "created_at": 1461116232227
-}
+  {
+    "user": {
+      "name": "Johann von Goethe",
+      "avatars": {
+        "small":   "https://vanillicon.com/d55cf8e18b47d4baaf60c006a0de39e1_50.png",
+        "regular": "https://vanillicon.com/d55cf8e18b47d4baaf60c006a0de39e1.png",
+        "large":   "https://vanillicon.com/d55cf8e18b47d4baaf60c006a0de39e1_200.png"
+      },
+      "handle": "@johann49"
+    },
+    "content": {
+      "text": "Es ist nichts schrecklicher als eine tätige Unwissenheit."
+    },
+    "created_at": 1461113796368
+  }
+];
